5419cb5d8dfe9c6be29cdaf631ccc4b6619a6d99
yihengd <2833411511@qq.com>
2025-03-16 18:32:03 +0800

    commit

--- train.sh
+++ train.sh
@@ -3,7 +3,11 @@ python train.py -e 150 \
                 --workdir ./workdir \
                 --model CoPA \
                 --dataset PH2 \
+<<<<<<< HEAD
                 --datapath ./data/PH2 \
+=======
+                --datapath ./data \
+>>>>>>> 6b747ba (commit message)
                 --unique_name train \
                 --save_epoch 50 \
                 --seed 304
