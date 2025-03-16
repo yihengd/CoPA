df014c270e02b875a7e6b400eb4086a5120b59c2
yihengd <80408810+yihengd@users.noreply.github.com>
2025-03-16 18:18:59 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python train.py -e 150 \
                 -b 32 \
-                --workdir /data/dyh/CoPA \
+                --workdir ./workdir \
                 --model CoPA \
                 --dataset PH2 \
-                --datapath /data/dyh/PH2 \
+                --datapath ./data/PH2 \
                 --unique_name train \
                 --save_epoch 50 \
                 --seed 304
